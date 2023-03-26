aca8a28610094bb71f9a99c4ef975077b423fbd6
Annashevchuk <sevcukanna960@gmail.com>
2023-03-26 20:22:25 +0300

    ex_3

--- main.cpp
+++ main.cpp
@@ -1,26 +1,27 @@
 #include <iostream>
 using namespace std;
-
-int factorial(int n);
-
+int number(int n);
 int main()
 {
     int n;
     cout << "Enter a number: ";
     cin >> n;
-    int result = factorial(n);
-    cout << "Factorial of " << n << " is " << result << endl;
+    int result = number(n);
+    cout << "Number " << n << " is " << (result ? "simple" : "not simple") << endl;
     return 0;
 }
 
-int factorial(int n)
+int number(int n)
 {
-    if (n == 0)
     {
-        return 1;
-    }
-    else
-    {
-        return n * factorial(n - 1);
+        for (int i = 2; i <= n / 2; ++i) 
+        {
+            if (n % i == 0) 
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
