07d1d98d988a47524c00a63f93bcd0e9dc54e6b4
Annashevchuk <sevcukanna960@gmail.com>
2023-03-28 16:46:46 +0300

    ex_3

--- main.cpp
+++ main.cpp
@@ -2,25 +2,22 @@
 
 using namespace std;
 
-int sum(int n)
+int sum_array(int arr[], int n) 
 {
-    if (n <= 0)
+    if (n == 0) 
     {
         return 0;
     }
-    else
+    else 
     {
-        return n + sum(n - 1);
+        return arr[n - 1] + sum_array(arr, n - 1);
     }
 }
 
-int main()
-{
-    int n;
-    cout << "Number: ";
-    cin >> n;
-    cout << "Sum " << n << ": ";
-    cout << sum(n) << " ";
-    cout << endl;
+int main() {
+    int arr[] = { 1, 2, 3, 4, 5 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int sum = sum_array(arr, n);
+    cout << "Sum of array elements is " << sum << endl;
     return 0;
 }
