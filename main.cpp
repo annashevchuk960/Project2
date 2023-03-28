ef5941461ac39a6862fc93dfd64a40de227084d9
Annashevchuk <sevcukanna960@gmail.com>
2023-03-28 16:24:11 +0300

    ex_3

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int sum(int n)
+{
+    if (n <= 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return n + sum(n - 1);
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Number: ";
+    cin >> n;
+    cout << "Sum " << n << ": ";
+    cout << sum(n) << " ";
+    cout << endl;
+    return 0;
+}
+
